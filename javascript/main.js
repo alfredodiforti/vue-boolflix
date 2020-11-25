461f391605b552932762cfa5637274970c277f12
Alfredo Di Forti <adf.palermo@gmail.com>
2020-11-25 16:18:50 +0100

    mancano solo le bandiere

--- javascript/main.js
+++ javascript/main.js
@@ -4,9 +4,35 @@ const app = new Vue({
     el:"#app",
     data: {
         catalog: [],
+        catalogtv: [],
         research: '',
     },
     methods: {
+        request() {
+            this.requestmovie();
+            this.requesttv();
+        },
+
+
+
+        requesttv() {
+            axios.get('https://api.themoviedb.org/3/search/tv', {
+                params: {
+                    api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    query: this.research ,
+                    language:'it-IT',
+                },
+            })
+            .then( response => {
+            this.catalogtv = response.data.results;
+            console.log(this.catalogtv);
+            })
+            .catch( error => {
+            console.log(error);
+            })
+        // end request series    
+        },
+        
         requestmovie() {
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
@@ -23,6 +49,10 @@ const app = new Vue({
             console.log(error);
             })
     // end requestmovie
+        },
+        getvote(vote) {
+            return Math.ceil(vote / 2);    
+    // end getvote         
         },
     // end methods
     },
